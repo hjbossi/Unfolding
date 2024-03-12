f571907c266ad6ed23bf5f858739d02e4d483b44
mitaylor <mitay@mit.edu>
2024-03-12 11:53:29 -0400

    Unfolding: run everything for all data and systematics

--- FitAll.sh
+++ FitAll.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 Tag=${1}
+Date=${2}
+
 Type=kErrors
 
-mkdir -p Output/$Tag/Bayes/Flat/$Type
-mkdir -p Output/$Tag/Bayes/MC/$Type
+mkdir -p /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/Bayes/MC/$Type/
+mkdir -p /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/SVD/MC/$Type/
+mkdir -p /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/Bayes/Flat/$Type/
+mkdir -p /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/SVD/Flat/$Type/
 
-mkdir -p log/$Tag/Bayes/Flat/$Type
-mkdir -p log/$Tag/Bayes/MC/$Type
+mkdir -p /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/Bayes/MC/$Type/
+mkdir -p /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/SVD/MC/$Type/
+mkdir -p /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/Bayes/Flat/$Type/
+mkdir -p /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/SVD/Flat/$Type/
 
 echo Running $Tag
 
-for i in `ls Input/$Tag/`
+for i in `ls /data/submit/mitay/unfolding/${Date}/Input/Data/$Tag/`
 do
-   echo $i
-    ./Execute --Input Input/$Tag/$i --Output Output/$Tag/Bayes/MC/$Type/$i --Prior MC --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit true --Error $Type > log/$Tag/Bayes/MC/$Type/$i.txt 2>&1  &
-    ./Execute --Input Input/$Tag/$i --Output Output/$Tag/Bayes/Flat/$Type/$i --Prior Flat --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit true --Error $Type > log/$Tag/Bayes/Flat/$Type/$i.txt 2>&1  &
+    echo $i
+    echo "./Execute --Input /data/submit/mitay/unfolding/${Date}/Input/Data/$Tag/$i --Output /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/Bayes/MC/$Type/$i --Prior MC --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/Bayes/MC/$Type/$i.txt 2>&1  &"
+    # ./Execute --Input /data/submit/mitay/unfolding/${Date}/Input/Data/$Tag/$i --Output /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/Bayes/MC/$Type/$i --Prior MC --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/Bayes/MC/$Type/$i.txt 2>&1  &
+    echo "./Execute --Input /data/submit/mitay/unfolding/${Date}/Input/Data/$Tag/$i --Output /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/SVD/MC/$Type/$i --Prior MC --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/SVD/MC/$Type/$i.txt 2>&1  &"
+    # ./Execute --Input /data/submit/mitay/unfolding/${Date}/Input/Data/$Tag/$i --Output /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/SVD/MC/$Type/$i --Prior MC --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/SVD/MC/$Type/$i.txt 2>&1  &
+    echo "./Execute --Input /data/submit/mitay/unfolding/${Date}/Input/Data/$Tag/$i --Output /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/Bayes/Flat/$Type/$i --Prior Flat --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/Bayes/Flat/$Type/$i.txt 2>&1  &"
+    # ./Execute --Input /data/submit/mitay/unfolding/${Date}/Input/Data/$Tag/$i --Output /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/Bayes/Flat/$Type/$i --Prior Flat --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/Bayes/Flat/$Type/$i.txt 2>&1  &
+    echo "./Execute --Input /data/submit/mitay/unfolding/${Date}/Input/Data/$Tag/$i --Output /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/SVD/Flat/$Type/$i --Prior Flat --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/SVD/Flat/$Type/$i.txt 2>&1  &"
+    # ./Execute --Input /data/submit/mitay/unfolding/${Date}/Input/Data/$Tag/$i --Output /data/submit/mitay/unfolding/${Date}/Output/Data/$Tag/SVD/Flat/$Type/$i --Prior Flat --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${Date}/Output/Data/log/$Tag/SVD/Flat/$Type/$i.txt 2>&1  &
 done
