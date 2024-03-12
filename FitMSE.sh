d716fed616f75dac0ee051d52e7dc8047bf89d59
mitaylor <mitay@mit.edu>
2024-03-12 10:23:48 -0400

    Unfolding: update paths to use data

--- FitMSE.sh
+++ FitMSE.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 Tag=${1}
+Date=${2}
+
 Type=kErrors
 
-mkdir -p Output/$Tag/Bayes/MC/$Type/MSE
-mkdir -p Output/$Tag/SVD/MC/$Type/MSE
-mkdir -p Output/$Tag/Bayes/Flat/$Type/MSE
-mkdir -p Output/$Tag/SVD/Flat/$Type/MSE
+mkdir -p /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/Bayes/MC/$Type/
+mkdir -p /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/SVD/MC/$Type/
+mkdir -p /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/Bayes/Flat/$Type/
+mkdir -p /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/SVD/Flat/$Type/
 
-mkdir -p log/$Tag/Bayes/MC/$Type/MSE
-mkdir -p log/$Tag/SVD/MC/$Type/MSE
-mkdir -p log/$Tag/Bayes/Flat/$Type/MSE
-mkdir -p log/$Tag/SVD/Flat/$Type/MSE
+mkdir -p /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/Bayes/MC/$Type/
+mkdir -p /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/SVD/MC/$Type/
+mkdir -p /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/Bayes/Flat/$Type/
+mkdir -p /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/SVD/Flat/$Type/
 
 echo Running $Tag
 
 for i in `ls Theory/$Tag/`
 do
 #    echo $i
-    echo "./ExecuteMSE --Input Theory/$Tag/$i --Output Output/$Tag/Bayes/MC/$Type/MSE/$i --Prior MC --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > log/$Tag/Bayes/MC/$Type/MSE/$i.txt 2>&1  &"
-    # ./ExecuteMSE --Input Theory/$Tag/$i --Output Output/$Tag/Bayes/MC/$Type/MSE/$i --Prior MC --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > log/$Tag/Bayes/MC/$Type/MSE/$i.txt 2>&1  &
-    echo "./ExecuteMSE --Input Theory/$Tag/$i --Output Output/$Tag/SVD/MC/$Type/MSE/$i --Prior MC --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > log/$Tag/SVD/MC/$Type/MSE/$i.txt 2>&1  &"
-    # ./ExecuteMSE --Input Theory/$Tag/$i --Output Output/$Tag/SVD/MC/$Type/MSE/$i --Prior MC --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > log/$Tag/SVD/MC/$Type/MSE/$i.txt 2>&1  &
-    echo "./ExecuteMSE --Input Theory/$Tag/$i --Output Output/$Tag/Bayes/Flat/$Type/MSE/$i --Prior Flat --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > log/$Tag/Bayes/Flat/$Type/MSE/$i.txt 2>&1  &"
-    # ./ExecuteMSE --Input Theory/$Tag/$i --Output Output/$Tag/Bayes/Flat/$Type/MSE/$i --Prior Flat --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > log/$Tag/Bayes/Flat/$Type/MSE/$i.txt 2>&1  &
-    echo "./ExecuteMSE --Input Theory/$Tag/$i --Output Output/$Tag/SVD/Flat/$Type/MSE/$i --Prior Flat --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > log/$Tag/SVD/Flat/$Type/MSE/$i.txt 2>&1  &"
-    # ./ExecuteMSE --Input Theory/$Tag/$i --Output Output/$Tag/SVD/Flat/$Type/MSE/$i --Prior Flat --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > log/$Tag/SVD/Flat/$Type/MSE/$i.txt 2>&1  &
+    echo "./ExecuteMSE --Input /data/submit/mitay/unfolding/${date}/Input/Theory/$Tag/$i --Output /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/Bayes/MC/$Type/$i --Prior MC --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/Bayes/MC/$Type/$i.txt 2>&1  &"
+    # ./ExecuteMSE --Input /data/submit/mitay/unfolding/${date}/Input/Theory/$Tag/$i --Output /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/Bayes/MC/$Type/$i --Prior MC --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/Bayes/MC/$Type/$i.txt 2>&1  &
+    echo "./ExecuteMSE --Input /data/submit/mitay/unfolding/${date}/Input/Theory/$Tag/$i --Output /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/SVD/MC/$Type/$i --Prior MC --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/SVD/MC/$Type/$i.txt 2>&1  &"
+    # ./ExecuteMSE --Input /data/submit/mitay/unfolding/${date}/Input/Theory/$Tag/$i --Output /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/SVD/MC/$Type/$i --Prior MC --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/SVD/MC/$Type/$i.txt 2>&1  &
+    echo "./ExecuteMSE --Input /data/submit/mitay/unfolding/${date}/Input/Theory/$Tag/$i --Output /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/Bayes/Flat/$Type/$i --Prior Flat --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/Bayes/Flat/$Type/$i.txt 2>&1  &"
+    # ./ExecuteMSE --Input /data/submit/mitay/unfolding/${date}/Input/Theory/$Tag/$i --Output /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/Bayes/Flat/$Type/$i --Prior Flat --DoBayes true --DoRepeatedBayes false --DoSVD false --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/Bayes/Flat/$Type/$i.txt 2>&1  &
+    echo "./ExecuteMSE --Input /data/submit/mitay/unfolding/${date}/Input/Theory/$Tag/$i --Output /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/SVD/Flat/$Type/$i --Prior Flat --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/SVD/Flat/$Type/$i.txt 2>&1  &"
+    # ./ExecuteMSE --Input /data/submit/mitay/unfolding/${date}/Input/Theory/$Tag/$i --Output /data/submit/mitay/unfolding/${date}/Output/Theory/$Tag/SVD/Flat/$Type/$i --Prior Flat --DoBayes false --DoRepeatedBayes false --DoSVD true --DoTSVD false --DoInvert false --DoTUnfold false --DoFit false --Error $Type > /data/submit/mitay/unfolding/${date}/Output/Theory/log/$Tag/SVD/Flat/$Type/$i.txt 2>&1  &
 done
