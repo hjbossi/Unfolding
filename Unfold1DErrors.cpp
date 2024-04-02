8318ba57947a28698e3e669a3e8812906bead11c
mitaylor <mitay@mit.edu>
2024-04-02 12:55:10 -0400

    Unfolding: add covariance calculation

--- Unfold1DErrors.cpp
+++ Unfold1DErrors.cpp
@@ -222,7 +222,7 @@ int main(int argc, char *argv[])
 
    int NGen = HResponse->GetNbinsY();
    int NReco = HResponse->GetNbinsX();
-   int NA = 100;
+   int NA = 1000;
 
    RemoveOutOfRange(HMeasured);
    RemoveOutOfRange(HTruth);
