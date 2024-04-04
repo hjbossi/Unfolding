6bbea377ac23a20d3ec4e58e888c28f1d375e454
mitaylor <mitay@mit.edu>
2024-04-04 11:01:15 -0400

    Unfolding: try to solve issue with SVD

--- Unfold1DErrors.cpp
+++ Unfold1DErrors.cpp
@@ -222,7 +222,7 @@ int main(int argc, char *argv[])
 
    int NGen = HResponse->GetNbinsY();
    int NReco = HResponse->GetNbinsX();
-   int NA = 2500;
+   int NA = (DoSVD) ? 1500 : 2500;
 
    RemoveOutOfRange(HMeasured);
    RemoveOutOfRange(HTruth);
